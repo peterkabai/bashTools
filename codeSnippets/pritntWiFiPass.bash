fc2ca3975f441d3504cbcab98cbb08b9426d9a1e
Peter <pkabai@csumb.edu>
2018-12-16 21:29:56 -0800

    Adds first code snippet

---
+++ codeSnippets/pritntWiFiPass.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# prints the wifi network
+wifiName=$(networksetup -getairportnetwork en1 | cut -c 24-)
+wifiPassword=$(security find-generic-password -wa $wifiName)
+echo $wifiPassword
