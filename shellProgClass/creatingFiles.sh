3e7d24a8339bc39f7b216a0772d8afae3fb57413
Peter <pkabai@csumb.edu>
2018-12-30 09:51:05 -0800

    Creates files

---
+++ shellProgClass/creatingFiles.sh
@@ -0,0 +1,17 @@
+# create a blank file
+touch testFiles/empty.txt
+
+# save calander to a file
+cal > testFiles/currentMonth.txt
+
+# save the list of useres to a file
+who > testFiles/users.txt
+
+# save the date to a new file
+date > testFiles/data.txt
+
+# save the output of a command to a file
+cat testFiles/text.txt > testFiles/textDuplicate.txt
+
+# change the date modified of the file
+touch -t 200301310846.26 testFiles/textDuplicate.txt
