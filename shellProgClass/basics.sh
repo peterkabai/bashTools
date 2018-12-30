095db741fc9f3c5c3190bc311c9ca911be8cdf4d
Peter <pkabai@csumb.edu>
2018-12-30 09:51:33 -0800

    Shell basics

---
+++ shellProgClass/basics.sh
@@ -0,0 +1,18 @@
+# show who's logged in
+who
+
+# copy and move files
+cp testFiles/original.txt testFiles/copy.txt
+mv testFiles/copy.txt testFiles/copied.txt
+
+# show the top two lines of a file
+head -n 2 testFiles/text.txt
+
+# show the last two lines of a file
+tail -n 2 testFiles/text.txt
+
+# printing text
+echo "Hello, World"
+
+# show the content of a file
+cat testFiles/text.txt
