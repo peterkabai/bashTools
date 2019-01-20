5253665d9e362d742e0873eac9c9f940f185e16b
Peter <pkabai@csumb.edu>
2019-01-20 08:37:26 -0800

    Adds a bunch of set functions

--- philipsHue/set.sh
+++ philipsHue/set.sh
@@ -2,9 +2,11 @@
 
 source api.sh
 
-# sets color temperature, ranges between 2200 and 6500 in K
-setTemp() { 
-	echo "Not yet implimented"
+# sets color temperature to a specific warmth
+setTemp() {
+	# temperature ranges between 2200K and 6500K
+	ct=$(echo $((($1-2200)*(500-154)/(6500-2200)+154)))
+	put '{"ct":'$ct'}' $2
 }
 
 # sets the transition speed in seconds
@@ -12,34 +14,46 @@ setSpeed() {
 	put '{"transitiontime":'$2'}' $1
 }
 
-# sets hue, ranges from 0 to 360
+# sets hue to a color
 setHue() {
-	echo "Not yet implimented"
+	# hue ranges from 0 to 360
+	hue=$(echo $(($2*65535/360)))
+	put '{"hue":'$hue'}' $1
 }
 
-# sets brightness, ranges from 0 to 100
+# sets brightness, 0 to 100
 setBrightness() {
-	echo "Not yet implimented"
+	bri=$(echo $(($2*254/100)))
+	put '{"bri":'$bri'}' $1
 }
 
-# sets the hue saturation, ranges from 0 to 100
+# sets the hue saturation, 0 to 100
 setSaturation() {
-	echo "Not yet implimented"
+	sat=$(echo $(($2*254/100)))
+	put '{"sat":'$sat'}' $1
 }
 
 # turns the light off if on, and vise versa
 toggleState() {
-	echo "Not yet implimented"
+	state=$(lightIsOn $1)
+	if [ $state == 'false' ]
+	then
+		turnOn $1
+		echo "Light" $1 "turned on"
+	else
+		turnOff $1
+		echo "Light" $1 "turned off"
+	fi
 }
 
 # turns the selected light on
 turnOn() {
-	echo "Not yet implimented"
+	put '{"on":true}' $1
 }
 
 # turns the selected light off
 turnOff() {
-	echo "Not yet implimented"
+	put '{"on":false}' $1
 }
 
 # loops through colors for a diven duration
