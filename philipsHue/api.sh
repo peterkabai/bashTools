9615abf4efafc8af72529ecd54afc5619019ff93
Peter <pkabai@csumb.edu>
2019-01-20 08:35:21 -0800

    Removes api key file

--- philipsHue/api.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# this file is not to be updated in git
-
-# local ip of the bridge
-ip="xxx.xxx.x.x"
-
-# api key, which is the philips username
-key="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
